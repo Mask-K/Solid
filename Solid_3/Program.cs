56b4c5219c45e6b7317ee153309ef49222041009
user <user@TEMP1>
2021-11-22 20:31:54 +0200

    Добавьте файлы проекта.

---
+++ Solid_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+interface IFigure
+{
+    int GetArea();
+}
+
+class Rectangle : IFigure
+{
+    public  int Width { get; set; }
+    public  int Height { get; set; }
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!!
+class Square : IFigure
+{
+    public int Width { get; set; }
+    public int GetArea()
+    {
+        return Width * Width;
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 10;
+
+            Console.WriteLine(rect.GetArea());
+            //Відповідь 100? Що не так???
+            Console.ReadKey();
+        }
+    }
---
+++ Solid_4/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/*Даний інтерфейс поганий тим, що він включає занадто багато методів.
+ А що, якщо наш клас товарів не може мати знижок або промокодом, або для нього немає сенсу встановлювати матеріал з 
+ якого зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в кожному класі невикористовувані в ньому методи, краще 
+розбити інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати потрібні інтерфейси.
+Перепишіть, розбивши інтерфейс на декілька інтерфейсів, керуючись принципом розділення інтерфейсу. 
+Опишіть класи книжки (розмір та колір не потрібні, але потрібна ціна та знижки) та верхній одяг (колір, розмір, ціна знижка),
+які реалізують притаманні їм інтерфейси.*/
+
+interface IItemPrice
+{
+    void SetPrice(double price);
+}
+
+interface IItemDiscount
+{
+    void ApplyDiscount(String discount);
+}
+interface IItemPromocode
+{
+    void ApplyPromocode(String promocode);
+}
+
+interface IItemSetColor
+{
+    void SetColor(byte color);
+}
+interface IItemSize
+{
+    void SetSize(byte size);
+}
+
+class Book : IItemPrice, IItemDiscount
+{
+    double Price { get; set; }
+    string Discount { get; set; }
+    public void SetPrice(double price)
+    {
+        Price = price;
+    }
+    public void ApplyDiscount(String discount)
+    {
+        Discount = discount;
+    }
+}
+
+class Surcoat : IItemSetColor, IItemSize, IItemPrice, IItemDiscount
+{
+    private double Price;
+    private byte Color;
+    private byte Size;
+    private string Discount;
+    public void SetPrice(double price)
+    {
+        Price = price;
+    }
+    public void ApplyDiscount(String discount)
+    {
+        Discount = discount;
+    }
+    public void SetColor(byte color)
+    {
+        Color = color;
+    }
+    public void SetSize(byte size)
+    {
+        Size = size;
+    }
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.ReadKey();
+    }
+}
